27e760b9d41ece6e21fab39a4d68af5974706cf6
Deepam <deepamprithyani@gmail.com>
2016-03-25 19:32:00 +0530

    first commit

---
+++ app/src/main/java/com/truce/testclevertap/MainActivity.java
@@ -0,0 +1,20 @@
+package com.truce.testclevertap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.clevertap.android.sdk.CleverTapAPI;
+import com.clevertap.android.sdk.exceptions.CleverTapMetaDataNotFoundException;
+import com.clevertap.android.sdk.exceptions.CleverTapPermissionsNotSatisfied;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
